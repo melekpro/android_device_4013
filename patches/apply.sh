6f8efcd669eacc9ad4574279db1cf49e5d05c481
Rayden_Berzerk409 <juandavidbogallo2020@gmail.com>
2024-03-15 15:44:09 -0500

    Fix some typos in apply.sh

--- patches/apply.sh
+++ patches/apply.sh
@@ -2,36 +2,30 @@
  cd ../../../..
 
  cd packages/apps/Settings
- patch -p1 -b < ../../../device/4013/patches/android_settings_developer_info.patch
+ patch -p1 -b < ../../../device/alcatel/4013/patches/android_settings_developer_info.patch
  git clean -f -d
 
  cd ../../../system/vold
- patch -p1 < ../../device/4013/patches/android_system_vold.patch
+ patch -p1 < ../../device/alcatel/4013/patches/android_system_vold.patch
  
  cd ../sepolicy
- patch -p1 < ../../device/4013/patches/android_system_sepolicy.patch
+ patch -p1 < ../../device/alcatel/4013/patches/android_system_sepolicy.patch
  cd ../../external/icu
- patch -p1 < ../../device/4013/patches/android_external_icu.patch
+ patch -p1 < ../../device/alcatel/4013/patches/android_external_icu.patch
 
  cd ../../frameworks/av
- patch -p1 < ../../device/4013/patches/android_frameworks_av.patch
- mkdir ../../device/4013/backup
+ patch -p1 < ../../device/alcatel/4013/patches/android_frameworks_av.patch
+ mkdir ../../device/alcatel/4013/backup
 
  cd ../..
  cd packages/apps/FMRadio
- patch -p1 < ../../../device/4013/patches/android_packages_apps_FMRadio.patch
+ patch -p1 < ../../../device/alcatel/4013/patches/android_packages_apps_FMRadio.patch
 
  cd ../../../
  
- cd vendor*/code*/telepho*
- cd ims/src/org/codeaurora/ims/utils/
- cp QtiImsExtUtils.java ../../../../../../../../../device/4013/backup/QtiImsExtUtils.java
- sudo rm -rf QtiImsExtUtils.java
- cp ../../../../../../../../../device/4013/patches/QtiImsExtUtils.java QtiImsExtUtils.java
- cd ../../../../../../../../../
  cd system/sepolicy
- cp mediaserver.te ../../device/4013/backup/mediaserver.te
+ cp mediaserver.te ../../device/alcatel/4013/backup/mediaserver.te
  sudo rm -rf mediaserver.te
- cp ../../device/4013/patches/mediaserver.te mediaserver.te
+ cp ../../device/alcatel/4013/patches/mediaserver.te mediaserver.te
  echo "Patch Success!"
- echo "IF Any Build Error, Restore the backups in repo/device/4013/backup"
+ echo "IF Any Build Error, Restore the backups in repo/device/alcatel/4013/backup"
